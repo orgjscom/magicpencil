cbdde01624b8fb1ea0905009c54dfac8c795332e
san4osq <san4osq@ya.ru>
2015-04-27 06:21:29 +0300

    mechanic draw pencil

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,8 @@
+$(function(){
+
+    $('#canvas').sketch();
+
+});
 (function() {
     var requestAnimationFrame = window.requestAnimationFrame ||
         window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame ||
